05b0a5f56b1049130f1a5207b46f53b0c99ef9ab
Li Zheng <flyskywhy@gmail.com>
2019-03-06 09:23:19 +0800

    fix(react-native-bt-telink): workaround 修复 telink SDK BluetoothLightLib 中时间获取年份使用 big endian ，而固件一上电第一次发过来的 1970 年使用 little endian 的 BUG

--- android/src/main/java/com/telink/bluetooth/light/GetTimeNotificationParser.java
+++ android/src/main/java/com/telink/bluetooth/light/GetTimeNotificationParser.java
@@ -29,7 +29,12 @@ public final class GetTimeNotificationParser extends NotificationParser<Calendar
         byte[] params = notifyInfo.params;
         int offset = 0;
 
-        int year = ((params[offset++] & 0xFF) << 8) + (params[offset++] & 0xFF);
+        int params0 = params[offset++] & 0xFF;
+        int params1 = params[offset++] & 0xFF;
+        int year = (params0 << 8) + params1;
+        if (params0 > params1) {
+            year = (params1 << 8) + params0;
+        }
         int month = (params[offset++] & 0xFF) - 1;
         int day = params[offset++] & 0xFF;
         int hour = params[offset++] & 0xFF;
