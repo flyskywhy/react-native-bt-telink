56a51c11e82fcdad7d54001d4d23c9354f838654
Li Zheng <flyskywhy@gmail.com>
2019-09-25 13:15:52 +0800

    refactor(react-native-bt-telink): 将 AES 加解密功能由 .so 中移到 .java 中，但还是没有解决 64 位 APK 运行时 OTA 100% 但 telink 蓝牙设备重启后还是原版本号没变的问题

--- android/src/main/java/com/telink/crypto/AES.java
+++ android/src/main/java/com/telink/crypto/AES.java
@@ -1,22 +1,22 @@
 /********************************************************************************************************
- * @file     AES.java
+ * @file AES.java
  *
- * @brief    for TLSR chips
+ * @brief for TLSR chips
  *
- * @author	 telink
- * @date     Sep. 30, 2010
+ * @author telink
+ * @date Sep. 30, 2010
  *
- * @par      Copyright (c) 2010, Telink Semiconductor (Shanghai) Co., Ltd.
+ * @par Copyright (c) 2010, Telink Semiconductor (Shanghai) Co., Ltd.
  *           All rights reserved.
  *
- *			 The information contained herein is confidential and proprietary property of Telink
- * 		     Semiconductor (Shanghai) Co., Ltd. and is available under the terms
- *			 of Commercial License Agreement between Telink Semiconductor (Shanghai)
- *			 Co., Ltd. and the licensee in separate contract or the terms described here-in.
+ *			 The information contained herein is confidential and proprietary property of Telink 
+ * 		     Semiconductor (Shanghai) Co., Ltd. and is available under the terms 
+ *			 of Commercial License Agreement between Telink Semiconductor (Shanghai) 
+ *			 Co., Ltd. and the licensee in separate contract or the terms described here-in. 
  *           This heading MUST NOT be removed from this file.
  *
- * 			 Licensees are granted free, non-transferable use of the information in this
- *			 file under Mutual Non-Disclosure Agreement. NO WARRENTY of ANY KIND is provided.
+ * 			 Licensees are granted free, non-transferable use of the information in this 
+ *			 file under Mutual Non-Disclosure Agreement. NO WARRENTY of ANY KIND is provided. 
  *
  *******************************************************************************************************/
 /*
@@ -42,9 +42,6 @@ public abstract class AES {
 
     public static boolean Security = true;
 
-    static {
-        System.loadLibrary("TelinkCrypto");
-    }
 
     private AES() {
     }
@@ -81,23 +78,145 @@ public abstract class AES {
         return cipher.doFinal(content);
     }
 
-    public static byte[] encrypt(byte[] key, byte[] nonce, byte[] plaintext) {
-
-        if (!AES.Security)
-            return plaintext;
-
-        return encryptCmd(plaintext, nonce, key);
+    public static byte[] aes_att_encryption(byte[] key, byte[] content) {
+        try {
+            byte[] re = encrypt(key, content);
+            return Arrays.reverse(re);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
-    public static byte[] decrypt(byte[] key, byte[] nonce, byte[] plaintext) {
+    // session key 16
+    // iv 8
+    // data 20 (3 seqNo, 2 mic, 15 ps)
+    public static byte[] encrypt(byte[] key, byte[] iv, byte[] data) {
 
         if (!AES.Security)
-            return plaintext;
+            return data;
 
-        return decryptCmd(plaintext, nonce, key);
+//        byte[] result = data.clone();
+        final int micLen = 2;
+//        byte[] mic = new byte[2];
+        final int micIndex = 3;
+
+        final int len = 15;
+//        byte[] ps = new byte[len];
+        final int psIndex = 5;
+
+
+        byte[] r = new byte[16];
+        byte[] e = new byte[16];
+        System.arraycopy(iv, 0, r, 0, 8);
+        r[8] = (byte) len;
+        r = aes_att_encryption(key, r);
+
+        for (int i = 0; i < 15; i++) {
+            if (r != null) {
+                r[i & 15] ^= data[i + psIndex];
+            }
+
+            if ((i & 15) == 15 || i == len - 1) {
+                r = aes_att_encryption(key, r);
+            }
+        }
+
+        for (int i = 0; i < micLen; i++) {
+            if (r != null) {
+                data[i + micIndex] = r[i];
+            }
+        }
+
+        if (r != null) {
+            for (int i = 0; i < r.length; i++) {
+                r[i] = 0;
+            }
+        }
+
+        if (r != null) {
+            System.arraycopy(iv, 0, r, 1, 8);
+        }
+
+        for (int i = 0; i < len; i++) {
+            if ((i & 15) == 0) {
+                e = aes_att_encryption(key, r);
+                if (r != null) {
+                    r[0]++;
+                }
+            }
+            if (e != null) {
+                data[i + psIndex] ^= e[i & 15];
+            }
+        }
+        return data;
     }
 
-    private static native byte[] encryptCmd(byte[] packet, byte[] iv, byte[] sk);
+    public static byte[] decrypt(byte[] key, byte[] iv, byte[] data) {
+        if (!AES.Security)
+            return data;
 
-    private static native byte[] decryptCmd(byte[] packet, byte[] iv, byte[] sk);
+//        byte[] result = data.clone();
+
+        byte[] r = new byte[16];
+        byte[] e = new byte[16];
+
+        final int micIndex = 5;
+        int micLen = 2;
+        final int psIndex = 7;
+        int len = 13;
+
+        int i;
+
+        System.arraycopy(iv, 0, r, 1, 8);
+
+        for (i = 0; i < len; i++) {
+            if ((i & 15) == 0) {
+                e = aes_att_encryption(key, r);
+                r[0]++;
+            }
+            if (e != null) {
+                data[i + psIndex] ^= e[i & 15];
+            }
+        }
+
+        for (i = 0; i < r.length; i++) {
+            r[i] = 0;
+        }
+        System.arraycopy(iv, 0, r, 0, 8);
+
+        r[8] = (byte) (len & 0xFF);
+
+        r = aes_att_encryption(key, r);
+
+        for (i = 0; i < len; i++) {
+            if (r != null) {
+                r[i & 15] ^= data[i + psIndex];
+            }
+
+            if ((i & 15) == 15 || i == len - 1) {
+                r = aes_att_encryption(key, r);
+            }
+        }
+
+        for (i = 0; i < micLen; i++) {
+            if (r != null && data[i + micIndex] != r[i]) {
+                return null;            //Failed
+            }
+        }
+        return data;
+
+    }
 }
