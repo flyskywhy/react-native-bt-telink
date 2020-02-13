6601a3118c3c0f8ded34239efd0a7d2abd82f06f
Li Zheng <flyskywhy@gmail.com>
2020-02-13 15:37:52 +0800

    feat(效果模拟): 添加了“波浪”的反向运动效果“波上”

--- index.native.js
+++ index.native.js
@@ -409,6 +409,10 @@ class TelinkBt {
                                 NativeModule.sendCommand(0xF1, meshAddress, [scene, speed, color3.r, color3.g, color3.b, color3Bg.r, color3Bg.g, color3Bg.b], immediate);
                                 changed = true;
                                 break;
+                            case 27:
+                                NativeModule.sendCommand(0xF1, meshAddress, [scene, speed, color3.r, color3.g, color3.b, colorSequence, colorsLength, colorSequenceAndEnd], immediate);
+                                changed = true;
+                                break;
                             default:
                                 break;
                         }
